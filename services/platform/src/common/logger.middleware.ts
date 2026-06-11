38e570b8c5ff26af21fd5d39b237aae378644d30
havie2309 <havienguyen2007@gmail.com>
2026-06-11 16:54:35 +0700

    A-04: add JSON logger and correlation ID middleware

---
+++ services/platform/src/common/logger.middleware.ts
@@ -0,0 +1,38 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import { createLogger, format, transports } from 'winston';
+
+export const logger = createLogger({
+  format: format.combine(
+    format.timestamp(),
+    format.json()
+  ),
+  transports: [new transports.Console()],
+});
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const correlationId = req.headers['x-correlation-id'] as string || uuidv4();
+    req.headers['x-correlation-id'] = correlationId;
+    res.setHeader('x-correlation-id', correlationId);
+
+    logger.info({
+      message: 'incoming request',
+      correlationId,
+      method: req.method,
+      url: req.url,
+    });
+
+    res.on('finish', () => {
+      logger.info({
+        message: 'request completed',
+        correlationId,
+        statusCode: res.statusCode,
+      });
+    });
+
+    next();
+  }
+}
