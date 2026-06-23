1615db3cd29c3a24f3a476a5097974993da35357
Ha Dang <43571214+dung0-o@users.noreply.github.com>
2026-06-23 14:18:20 +0700

    feat(auth): Implement locking account after repeated failed login (#37)

--- services/platform/apps/user-management/src/user-management.module.ts
+++ services/platform/apps/user-management/src/user-management.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { JwtModule, JwtSignOptions } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 import { CommonModule } from '../../../src/common/common.module'
+import { RedisModule } from '../../../src/common/redis/redis.module'
 import { AuthController } from './auth/auth.controller'
 import { AuthService } from './auth/auth.service'
 import { JwtStrategy } from './auth/jwt.strategy'
@@ -13,6 +14,7 @@ import { UsersService } from './user/users.service'
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PassportModule,
+    RedisModule,
     JwtModule.registerAsync({
       global: true,
       inject: [ConfigService],
